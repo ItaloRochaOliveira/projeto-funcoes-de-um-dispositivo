569e984ed2f803b2a9336310a6383641629e147a
ItaloRochaOliveira <italo.rocha.de.oliveira@gmail.com>
2023-09-18 20:54:53 -0300

    trazendo o fluxo do dispositivo com algumas modificações na classe Reproduzir música

--- src/Dispositivo.java
+++ src/Dispositivo.java
@@ -1,12 +1,33 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import funcionalidades.ReproduzirMusica;
 
 public class Dispositivo {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public Dispositivo() throws InterruptedException {
+        System.out.println("Iniciando dispositivo...");
 
-        ReproduzirMusica appMusica = new ReproduzirMusica();
+        Thread.sleep(2000);
+    }
+
+    public static void main(String[] args) {
+
+        String[] opcoes = {"Reprodutor musical", "Aparelho telefônico", "Navegador", "Sair"};
+
+        System.out.println("Escolha o que deseja fazer com o aparelho:");
+
+        int escolha = imprimirOpcoes(opcoes);
+
+        while (escolha != 4) {
+            if(escolha == 1){
+                ReproduzirMusica appMusica = new ReproduzirMusica();
+            }
+
+
+            escolha = imprimirOpcoes(opcoes);
+        }
+
 
         
         // int opcoes = scanner.nextInt();
@@ -15,7 +36,18 @@ public class Dispositivo {
         //     appMusica.tocar();
         // }
 
-        appMusica.selecionarMusica();
+        // appMusica.selecionarMusica();
         
+    }
+
+    public static int imprimirOpcoes(String[] array){
+        Scanner scanner = new Scanner(System.in);
+
+        AtomicInteger index = new AtomicInteger();
+        Arrays.stream(array).map(str -> index.getAndIncrement() + 1 + " - " + str).forEach(System.out::println);
+
+        int escolha = scanner.nextInt();
+
+        return escolha;
     }
 }
